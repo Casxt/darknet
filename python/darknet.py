f5713282f31538738eee67aa31502bd8ca63a011
Yauhen Kharuzhy <jekhor@gmail.com>
2018-05-09 16:59:13 +0300

    Add args for configure net and set input image in darknet.py

--- python/darknet.py
+++ python/darknet.py
@@ -1,6 +1,7 @@
 from ctypes import *
 import math
 import random
+import sys
 
 def sample(probs):
     s = sum(probs)
@@ -45,7 +46,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -141,16 +142,24 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     free_image(im)
     free_detections(dets, num)
     return res
-    
+
 if __name__ == "__main__":
     #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
+    if len(sys.argv) != 5:
+        print "Usage: darknet <data cfg> <net cfg> <weights> <input image>"
+        sys.exit(1)
+
+    data_cfg = sys.argv[1]
+    net_cfg = sys.argv[2]
+    net_weights = sys.argv[3]
+    filename = sys.argv[4]
+    net = load_net(net_cfg, net_weights, 0)
+    meta = load_meta(data_cfg)
+    r = detect(net, meta, filename)
     print r
-    
+
 
